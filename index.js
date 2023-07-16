750f69e7ad294fbeec171e469c324a1f6f972d69
Gabriel Abaya <gabrielabaya08@gmail,com>
2023-07-17 05:33:06 +0800

    Completed with Comment

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose")
 const loginRoute = require("./routes/loginRoute")
 const productRoute = require("./routes/productsRoute")
-const userOrderRoute = require("./routes/usersOrderRoute")
+const userOrderRoute = require("./routes/usersOrder")
 const app = express();
 const port = 4000
-
-
+// AN22 source code Template for 2T AY 2022-2023
+/*
+	Program:	Computation of Grades using Function
+	Programmer:	ABAYA, Jose Gabriel D.
+              MADRID, Francis Ludwick T.
+	Section:	AN22
+	Start Date:	July 15, 2023
+	End Date:	July 17, 2023
+*/
 app.use(express.json())
 mongoose.connect(
     "mongodb+srv://gabriel_202110419:ilovemongodb08@sandbox.glievxm.mongodb.net/Ecommerce?retryWrites=true&w=majority",
